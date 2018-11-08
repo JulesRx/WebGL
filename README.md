c1fa9ff20f10d3b3e0e38fc2395389524bd2b9cd
JulesRx <julesraffoux@gmail.com>
2018-11-08 17:32:35 +0100

    TP01

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebGL
 Cours WebGL DAWIN 2018-2019
+
+Sources :
+- http://www.labri.fr/perso/pbiasutt/Cours/WebGL/
+- http://www.labri.fr/perso/fgrelard/teaching.html
+- http://www.labri.fr/perso/fgrelard/ens/webgl/
--- TP/1/tp1.js
+++ TP/1/tp1.js
@@ -3,7 +3,7 @@ function loadText(url) {
     xhr.open('GET', url, false);
     xhr.overrideMimeType("text/plain");
     xhr.send(null);
-    if(xhr.status === 200)
+    if (xhr.status === 200)
         return xhr.responseText;
     else {
         return null;
@@ -15,8 +15,8 @@ var canvas;
 var gl; //contexte
 var program; //shader program
 var attribPos; //attribute position
-var attribSize; //attribute size
 var pointSize = 10.;
+var mousePositions = [[]];
 
 function initContext() {
     canvas = document.getElementById('dawin-webgl');
@@ -30,23 +30,68 @@ function initContext() {
 
 //Initialisation des shaders et du program
 function initShaders() {
+    var fragmentSource = loadText('fragment.glsl');
+    var vertexSource = loadText('vertex.glsl');
 
+    var fragment = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragment, fragmentSource);
+    gl.compileShader(fragment);
+
+    var vertex = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertex, vertexSource);
+    gl.compileShader(vertex);
+
+    gl.getShaderParameter(fragment, gl.COMPILE_STATUS);
+    gl.getShaderParameter(vertex, gl.COMPILE_STATUS);
+
+    if (!gl.getShaderParameter(fragment, gl.COMPILE_STATUS)) {
+        console.log(gl.getShaderInfoLog(fragment));
+    }
+
+    if (!gl.getShaderParameter(vertex, gl.COMPILE_STATUS)) {
+        console.log(gl.getShaderInfoLog(vertex));
+    }
+
+    program = gl.createProgram();
+    gl.attachShader(program, fragment);
+    gl.attachShader(program, vertex);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.log("Could not initialise shaders");
+    }
+    gl.useProgram(program);
 }
 
 //Fonction initialisant les attributs pour l'affichage (position et taille)
 function initAttributes() {
+    gl.bindAttribLocation(program, 0, "position");
+}
 
+//Evenement souris
+function initEvents() {
+    canvas.onclick = function (e) {
+        //changement de repere pour les coordonnees de souris
+        var mousePos = []
+        mousePos[0] = (e.pageX / canvas.width) * 2.0 - 1.0;
+        mousePos[1] = ((canvas.height - e.pageY) / canvas.height) * 2.0 - 1.0;
+        mousePositions.push(mousePos);
+        draw();
+    }
 }
 
 //Fonction permettant le dessin dans le canvas
 function draw() {
-
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    for (var i = 0; i < mousePositions.length; i++) {
+        gl.vertexAttrib2f(0, mousePositions[i][0], mousePositions[i][1]);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
 }
-
 
 function main() {
     initContext();
-    //initShaders();
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    //draw();
+    initShaders();
+    initAttributes();
+    initEvents();
+    draw();
 }
