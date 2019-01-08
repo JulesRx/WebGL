025fef6b01bd7af03fcc58f493d28c56096939f5
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-01-08 15:59:08 +0100

    Cleanup

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Sources :
 - http://www.labri.fr/perso/pbiasutt/Cours/WebGL/
 - http://www.labri.fr/perso/fgrelard/teaching.html
 - http://www.labri.fr/perso/fgrelard/ens/webgl/
+
+## Demo
+Dispo sur https://julesrx.github.io/webgl
---
+++ assets/glsl/vertex.glsl
@@ -0,0 +1,15 @@
+uniform mat4 perspective;
+uniform mat4 translation;
+uniform mat4 rotation;
+uniform mat4 scale;
+
+attribute vec3 position;
+attribute vec3 vertexColor;
+
+varying vec4 vColor;
+
+void main () {
+  gl_Position = perspective * translation * rotation * scale * vec4(position, 1);
+  gl_PointSize = 1.0;
+  vColor = vec4(vertexColor, 1.0);
+}
